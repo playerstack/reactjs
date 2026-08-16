3b0f1d66318fa3792b0ed825b0fceef89acad2f2
Oscar Garces <soyvillareal@gmail.com>
2026-08-16 14:09:58 -0500

    test: add SpritePreview and Tooltip coverage (#8)
    New test files increase overall coverage:
    - Statements: 93.84% (target 90%+)
    - Functions: 90.55%
    - Lines: 93.79%
    
    74 test suites, 1180 tests, all passing.

---
+++ test/playerSkin/Tooltip.spec.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+jest.mock('@playerstack/core', () => ({
+  ...jest.requireActual('@playerstack/core'),
+}));
+
+import Tooltip from '../../src/PlayerSkin/Commons/Tooltip';
+import { AppContextProvider } from '../../src/context/AppContextProvider';
+
+const Wrapper = ({ children }) => <AppContextProvider language="en">{children}</AppContextProvider>;
+
+describe('Tooltip', () => {
+  test('renders children', () => {
+    const { getByRole } = render(
+      <Wrapper>
+        <Tooltip label="Play">
+          <button>Play</button>
+        </Tooltip>
+      </Wrapper>,
+    );
+    expect(getByRole('button')).toBeDefined();
+  });
+
+  test('renders with label prop', () => {
+    const { container } = render(
+      <Wrapper>
+        <Tooltip label="Volume">
+          <button>Vol</button>
+        </Tooltip>
+      </Wrapper>,
+    );
+    expect(container).toBeDefined();
+  });
+
+  test('renders when disabled', () => {
+    const { getByText } = render(
+      <Wrapper>
+        <Tooltip label="Mute" disabled>
+          <button>Mute</button>
+        </Tooltip>
+      </Wrapper>,
+    );
+    expect(getByText('Mute')).toBeDefined();
+  });
+
+  test('renders in fullscreen mode', () => {
+    const { getByRole } = render(
+      <Wrapper>
+        <Tooltip label="FS" fullscreen>
+          <button>FS</button>
+        </Tooltip>
+      </Wrapper>,
+    );
+    expect(getByRole('button')).toBeDefined();
+  });
+});
